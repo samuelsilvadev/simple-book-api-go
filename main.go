31441b5b2a6b1700f6266aa3e190f9decbd2769e
Samuel Silva <samuelsilvawb@gmail.com>
2024-03-18 21:31:19 +0000

    feat: add checkout endpoint

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	db "simplebookapi/db"
 	"simplebookapi/models"
@@ -47,11 +49,49 @@ func createBook(context *gin.Context) {
 	context.IndentedJSON(http.StatusCreated, newBook)
 }
 
+func checkOutBook(context *gin.Context) {
+	id, ok := context.GetQuery("id")
+
+	if !ok {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	parsedId, _ := strconv.Atoi(id)
+
+	findBookById := func() (models.Book, int, error) {
+		for index, book := range db.Books {
+			if book.ID == parsedId {
+				return book, index, nil
+			}
+		}
+
+		return models.Book{}, -1, errors.New("book not found")
+	}
+
+	book, bookIndex, err := findBookById()
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		context.IndentedJSON(http.StatusConflict, gin.H{"message": "Book is not available"})
+		return
+	}
+
+	book.Quantity = book.Quantity - 1
+	db.Books[bookIndex] = book
+	context.IndentedJSON(http.StatusOK, book)
+}
+
 func main() {
 	fmt.Println("App is running...")
 
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.POST("/books", createBook)
+	router.PATCH("/books/checkout", checkOutBook)
 	router.Run(":8888")
 }
